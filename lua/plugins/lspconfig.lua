2fae5f3b824500573c3f118d3cb5cbbf58720bb2
Nuri UZUNOĞLU <nuriu@protonmail.com>
2024-01-18 22:52:04 +0300

    added rust lsp, updated lua_ls, omnisharp and clangd settings.

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -65,9 +65,11 @@ return {
         'stylua',
         'lua_ls',
         'markdownlint',
+        'prettierd',
         'omnisharp',
         'clangd',
         'gopls',
+        'rust-analyzer',
       },
     })
 
@@ -84,6 +86,8 @@ return {
       capabilities = capabilities,
       settings = {
         Lua = {
+          semantic = { enable = false },
+          hint = { enable = true },
           workspace = { checkThirdParty = false },
           telemetry = { enable = false },
           diagnostics = {
@@ -96,13 +100,84 @@ return {
 
     lspconfig.omnisharp.setup({
       capabilities = capabilities,
+
+      cmd = { 'omnisharp' },
+
+      -- Enables support for reading code style, naming convention and analyzer
+      -- settings from .editorconfig.
+      enable_editorconfig_support = true,
+
+      -- If true, MSBuild project system will only load projects for files that
+      -- were opened in the editor. This setting is useful for big C# codebases
+      -- and allows for faster initialization of code navigation features only
+      -- for projects that are relevant to code that is being edited. With this
+      -- setting enabled OmniSharp may load fewer projects and may thus display
+      -- incomplete reference lists for symbols.
+      enable_ms_build_load_projects_on_demand = false,
+
+      -- Enables support for roslyn analyzers, code fixes and rulesets.
+      enable_roslyn_analyzers = true,
+
+      -- Specifies whether 'using' directives should be grouped and sorted during
+      -- document formatting.
+      organize_imports_on_format = true,
+
+      -- Enables support for showing unimported types and unimported extension
+      -- methods in completion lists. When committed, the appropriate using
+      -- directive will be added at the top of the current file. This option can
+      -- have a negative impact on initial completion responsiveness,
+      -- particularly for the first few completion sessions after opening a
+      -- solution.
+      enable_import_completion = false,
+
+      -- Specifies whether to include preview versions of the .NET SDK when
+      -- determining which version to use for project loading.
+      sdk_include_prereleases = false,
+
+      -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
+      -- true
+      analyze_open_documents_only = true,
     })
 
     lspconfig.clangd.setup({
       capabilities = capabilities,
+      cmd = {
+        'clangd',
+        -- "--all-scopes-completion",
+        -- "--suggest-missing-includes",
+        -- "--background-index",
+        -- "--pch-storage=disk",
+        -- "--cross-file-rename",
+        -- "--log=info",
+        -- "--completion-style=detailed",
+        -- "--enable-config", -- clangd 11+ supports reading from .clangd configuration file
+        -- "--clang-tidy",
+        '--offset-encoding=utf-16',
+        -- "--clang-tidy-checks=-*,llvm-*,clang-analyzer-*,modernize-*,-modernize-use-trailing-return-type",
+        -- "--fallback-style=Google",
+        -- "--header-insertion=never",
+        -- "--query-driver=<list-of-white-listed-complers>"
+      },
     })
 
     lspconfig.gopls.setup({
+      capabilities = capabilities,
+      settings = {
+        gopls = {
+          hints = {
+            assignVariableTypes = true,
+            compositeLiteralFields = true,
+            compositeLiteralTypes = true,
+            constantValues = true,
+            functionTypeParameters = true,
+            parameterNames = true,
+            rangeVariableTypes = true,
+          },
+        },
+      },
+    })
+
+    lspconfig.rust_analyzer.setup({
       capabilities = capabilities,
     })
   end,
